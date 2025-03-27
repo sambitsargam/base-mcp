ccd243febe3f0d1c2ef47378308fbd063c8c7618
dschlabach <daniel.schlabach3@gmail.com>
2025-03-27 09:33:42 -0400

    fix: remove exports

--- src/tools/nft/index.ts
+++ src/tools/nft/index.ts
@@ -2,14 +2,6 @@ import { generateTool } from '../../utils.js';
 import { listNftsHandler, transferNftHandler } from './handlers.js';
 import { ListNftsSchema, TransferNftSchema } from './schemas.js';
 
-// Re-export utility functions for use elsewhere in the codebase
-export {
-  fetchNftsFromAlchemy,
-  formatNftData,
-  detectNftStandard,
-  transferNft,
-} from './utils.js';
-
 export const listNftsTool = generateTool({
   name: 'list_nfts',
   description: 'List NFTs owned by a specific address',
